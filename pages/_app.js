d15b8640c16c5808053912d758f5dbaba51f89ee
Theodorus Clarence <theodorusclarence@gmail.com>
2021-07-02 15:46:35 +0700

    chore: add react-hot-toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import '@/styles/globals.css';
 import 'react-datepicker/dist/react-datepicker.css';
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <div>
+        <Toaster
+          reverseOrder={false}
+          toastOptions={{
+            style: {
+              borderRadius: '8px',
+              background: '#333',
+              color: '#fff',
+            },
+          }}
+        />
+      </div>
       <Component {...pageProps} />
     </>
   );
